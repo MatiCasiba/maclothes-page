8fddda6461afd05bead9fd33170a567641b9b1a3
Matias Casiba <casibagabriel@gmail.com>
2026-03-03 14:01:25 -0300

    Armado del Layout y modificación en main.jsx (incluyo Router)

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,8 +1,19 @@
 
+import { Outlet } from 'react-router-dom';
+import Header from './header/Header';
+import Footer from './footer/Footer';
+import styles from './Layout.module.scss';
+
 const Layout = () => {
   return (
-    <div>Layout</div>
-  )
-}
+    <div className={styles.layout}>
+      <Header />
+      <main className={styles.main}>
+        <Outlet /> {/* renderizado de las páginas */}
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
