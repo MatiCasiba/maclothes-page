dd56344196088d867a47e7420be6be7bcf5518e4
Matias Casiba <casibagabriel@gmail.com>
2026-03-03 13:37:04 -0300

    Instalo react router y armado de rutas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,8 +1,21 @@
 
+import { Routes, Route } from 'react-router-dom';
+import Layout from '../components/layout/Layout';
+import Home from '../pages/Home/Home';
+import Contact from '../pages/Contact/Contact';
+import AboutUs from '../pages/AboutUs/AboutUs';
+
 const AppRoutes = () => {
   return (
-    <div>AppRoutes</div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="contacto" element={<Contact />} />
+        <Route path="nosotros" element={<AboutUs />} />
+        
+      </Route>
+    </Routes>
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
