6263c8e85014bbb15f722a189dd53643e89377cf
Matias Casiba <casibagabriel@gmail.com>
2026-03-03 13:38:32 -0300

    Correción de caracteres en AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,9 +1,9 @@
 
 import { Routes, Route } from 'react-router-dom';
 import Layout from '../components/layout/Layout';
-import Home from '../pages/Home/Home';
-import Contact from '../pages/Contact/Contact';
-import AboutUs from '../pages/AboutUs/AboutUs';
+import Home from '../pages/home/Home';
+import Contact from '../pages/contact/Contact';
+import AboutUs from '../pages/aboutUs/AboutUs';
 
 const AppRoutes = () => {
   return (
