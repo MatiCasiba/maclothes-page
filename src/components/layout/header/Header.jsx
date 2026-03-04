9ef65eb4dcdebe168692d06ad656f202e3772eb4
Matias Casiba <casibagabriel@gmail.com>
2026-03-04 12:56:05 -0300

    Armado del Header

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,7 +1,51 @@
+import { useState } from "react"
+import SearchBar from "./SearchBar"
+import CartIcon from "./CartIcon"
+import MenuToggle from "./MenuToggle"
+import styles from './Header.module.scss'
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return (
-    <div>Header</div>
+    <header className={styles.header}>
+      <div className={styles.headerContainer}>
+        {/* menú Hamburguesa */}
+        <div className={styles.menuToggle}>
+          <button 
+            className={styles.hamburger}
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            ☰
+          </button>
+        </div>
+
+        {/* logo */}
+        <div className={styles.logo}>
+          <a href="/">
+            <img src="/logo/logo.png" alt="Maclothes" />
+            {/* <p>Maclothes</p> */}
+          </a>
+        </div>
+
+        {/* buscador (visible en desktop, toggle en mobile) */}
+        <div className={styles.searchContainer}>
+          <SearchBar />
+        </div>
+
+        {/* carrito */}
+        <div className={styles.cartContainer}>
+          <CartIcon />
+        </div>
+      </div>
+
+      {/* menú desplegable (solo visible cuando isMenuOpen es true) */}
+      {isMenuOpen && (
+        <div className={styles.menuOverlay}>
+          <MenuToggle onClose={() => setIsMenuOpen(false)} />
+        </div>
+      )}
+    </header>
   )
 }
 
