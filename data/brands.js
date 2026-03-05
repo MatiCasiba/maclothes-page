86bd136fd97da427b1b0a5f540655bda38459631
Matias Casiba <casibagabriel@gmail.com>
2026-03-05 19:37:10 -0300

    Armo la data de marcas

---
+++ data/brands.js
@@ -0,0 +1,6 @@
+export const brands = [
+  { id: 1, name: 'macowens', logo: '/logo/macowens-esc.png' },
+  { id: 2, name: 'markova', logo: '/logo/markova.jpg' },
+  { id: 3, name: 'Zara', logo: '/logo/zara-logo.png' },
+  { id: 4, name: 'Prada', logo: '/logo/prada.webp' }
+];
