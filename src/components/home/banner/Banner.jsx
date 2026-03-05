fac4ef87abb85e53ebdde358c9b4f0c4114c95eb
Matias Casiba <casibagabriel@gmail.com>
2026-03-05 20:06:13 -0300

    Armado del banner y estilización

--- src/components/home/banner/Banner.jsx
+++ src/components/home/banner/Banner.jsx
@@ -1,7 +1,14 @@
+import styles from './Banner.module.scss'
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <section className={styles.banner}>
+      <div className={styles.bannerContent}>
+        <h1 className={styles.bannerTitle}>NUEVA COLECCIÓN</h1>
+        <p className={styles.bannerSubtitle}>Descubre las últimas tendencias</p>
+        <button className={styles.bannerButton}>Ver colección</button>
+      </div>
+    </section>
   )
 }
 
