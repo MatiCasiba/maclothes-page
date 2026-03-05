db33a69dc14c9ea3c1e37646db41cafbbcdcdca6
Matias Casiba <casibagabriel@gmail.com>
2026-03-05 19:24:10 -0300

    Armado de la página Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,19 @@
 
+import Banner from '../../components/home/banner/Banner';
+import BrandSlider from '../../components/home/brandsSlider/BrandSlider';
+import OfferSection from '../../components/home/offerSection/OfferSection';
+import Testimonials from '../../components/home/testimonials/Testimonials';
+import styles from './Home.module.scss';
+
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className={styles.home}>
+      <Banner />
+      <BrandSlider />
+      <OfferSection />
+      <Testimonials />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
