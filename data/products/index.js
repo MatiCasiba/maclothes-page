90be2fbbce7972f82b4c7578f795189f93526d5f
Matias Casiba <casibagabriel@gmail.com>
2026-03-08 10:06:34 -0300

    Armo el index.js para exportar todo

---
+++ data/products/index.js
@@ -0,0 +1,30 @@
+import { mujerProducts } from "./mujer/mujer";
+import { hombreProducts } from "./hombre/hombre"
+
+export const products = {
+    mujer: mujerProducts,
+    hombre: hombreProducts || [],
+    all: [...mujerProducts, ...(hombreProducts || [])]
+} 
+
+// helpper para filtrar por categoría
+export const getProductsByCategory = (category, subcategoria = null, tipo = null) => {
+    let filtered = products.all.filter(p => p.category === category)
+    if(subcategoria) {
+        filtered = filtered.filter(p => p.subcategoria === subcategoria)
+    }
+    if(tipo){
+        filtered = filtered.filter(p => p.tipo === tipo)
+    }
+    return filtered
+}
+
+// helper para productos en oferta
+export const getOfferProducts = () => {
+    return products.all.filter(p => p.offerPrice !== null)
+}
+
+// helper para productos destacados
+export const getFeaturedProducts = () => {
+    return products.all.filter(p => p.destacado)
+}
