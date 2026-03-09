42a4cbcc60c14d4dfd3ecf94e45a3fd886d824dd
Matias Casiba <casibagabriel@gmail.com>
2026-03-09 12:25:33 -0300

    Actualización en brand.js

--- data/brands.js
+++ data/brands.js
@@ -2,5 +2,7 @@ export const brands = [
   { id: 1, name: 'macowens', logo: '/logo/macowens-esc.png' },
   { id: 2, name: 'markova', logo: '/logo/markova.jpg' },
   { id: 3, name: 'Zara', logo: '/logo/zara-logo.png' },
-  { id: 4, name: 'Prada', logo: '/logo/prada.webp' }
+  { id: 4, name: 'Prada', logo: '/logo/prada.webp' },
+  { id: 5, name: 'gnz', logo: '/logo/gnz-logo.png'},
+  { id: 6, name: 'valkur', logo: '/logo/valkur-logo.webp'}
 ];
