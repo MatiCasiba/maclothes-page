9bf9aaa35c4852dce68d7a0243a7c1c7696581fd
Matias Casiba <casibagabriel@gmail.com>
2026-03-09 13:48:30 -0300

    Armo la data de los testimonios

---
+++ data/testimonials.js
@@ -0,0 +1,72 @@
+export const testimonials = [
+  {
+    id: 1,
+    name: 'María González',
+    photo: '/avatars/avatar-female.png',
+    rating: 5,
+    comment: 'Excelente calidad y atención. La ropa es tal cual se ve en las fotos. Compré un blazer para una entrevista y quedó perfecto. ¡Muy recomendable!'
+  },
+  {
+    id: 2,
+    name: 'Carlos Rodríguez',
+    photo: '/avatars/avatar-male.png',
+    rating: 4,
+    comment: 'Buenos precios y envío rápido. Las remeras son de muy buena calidad. Volveré a comprar sin dudas.'
+  },
+  {
+    id: 3,
+    name: 'Laura Martínez',
+    photo: '/avatars/avatar-female.png',
+    rating: 5,
+    comment: 'Me encanta la variedad de estilos que tienen. Compré un vestido para un casamiento y recibí muchísimos cumplidos. ¡Gracias Maclothes!'
+  },
+  {
+    id: 4,
+    name: 'Javier López',
+    photo: '/avatars/avatar-male.png',
+    rating: 5,
+    comment: 'La camisa de lino que compré es súper fresca y de gran calidad. La atención al cliente fue excelente cuando tuve una duda sobre talles.'
+  },
+  {
+    id: 5,
+    name: 'Ana Sánchez',
+    photo: '/avatars/avatar-female.png',
+    rating: 4,
+    comment: 'Muy linda ropa y los precios son accesibles. Compré unos jeans y me quedaron perfectos. Según la guía de talles acerté de lleno.'
+  },
+  {
+    id: 6,
+    name: 'Diego Pérez',
+    photo: '/avatars/avatar-male.png',
+    rating: 5,
+    comment: 'Compré un sweater y es súper suave y abrigado. El envío llegó antes de lo esperado. Muy contento con mi compra.'
+  },
+  {
+    id: 7,
+    name: 'Valentina Díaz',
+    photo: '/avatars/avatar-female.png',
+    rating: 5,
+    comment: 'Las buzos son hermosos y la tela es de calidad. Además el local tiene muy buena onda. Recomiendo 100%'
+  },
+  {
+    id: 8,
+    name: 'Pablo Fernández',
+    photo: '/avatars/avatar-male.png',
+    rating: 4,
+    comment: 'Buena relación precio-calidad. Compré zapatillas y son cómodas. El único detalle es que demoraron un par de días más, pero me avisaron.'
+  },
+  {
+    id: 9,
+    name: 'Camila Torres',
+    photo: '/avatars/avatar-female.png',
+    rating: 5,
+    comment: 'Me encanta Maclothes. Siempre encuentro ropa linda y a buen precio. El sistema de cambios es muy sencillo.'
+  },
+  {
+    id: 10,
+    name: 'Martín Acosta',
+    photo: '/avatars/avatar-male.png',
+    rating: 5,
+    comment: 'Compré un pantalón de vestir para una boda y me quedó impecable. La calidad es superior a lo que esperaba por el precio.'
+  }
+];
