4451bbeb690e3ade54ec68bcaf9149e5e41926a0
Matias Casiba <casibagabriel@gmail.com>
2026-03-28 12:43:00 -0300

    Armo el loader de la página (animación de carga)

--- src/components/common/loader/Loader.jsx
+++ src/components/common/loader/Loader.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react"
+import styles from "./Loader.module.scss"
 
 const Loader = () => {
+  const [isVisible, setIsVisible] = useState(true)
+
+  useEffect(() => {
+    //oculto el loader después de que la página cargue
+    const timer = setTimeout(() => {
+        setIsVisible(false)
+    }, 1500) // 1.5 segundos
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  if(!isVisible) return null
+
   return (
-    <div>Loader</div>
+    <div className={styles.loaderOverlay}>
+      <div className={styles.loaderContainer}>
+        <div className={styles.logoLoader}>MACLOTHES</div>
+        <div className={styles.spinner}></div>
+      </div>
+    </div>
   )
 }
 
